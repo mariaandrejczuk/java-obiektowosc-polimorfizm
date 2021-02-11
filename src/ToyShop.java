bca5882b70a40af87b223958557f73ac761e8b2e
Maria Andrejczuk <Maria.Andrejczuk@gmail.com>
2021-02-11 13:05:55 +0100

    #1 stworzenie ogólnej tablicy dla toys, modyfikacja wyświetlania

--- src/ToyShop.java
+++ src/ToyShop.java
@@ -1,32 +1,40 @@
 
 public class ToyShop {  //miejsce do przechowywania wszystkich zabawek
 
-    private Bear[] bears = new Bear[10];
-    private int indexBear = 0;
+    //private Bear[] bears = new Bear[10];
+    //private int indexBear = 0;
 
-    private Car[] cars = new Car[10];
-    private int indexCar = 0;
+    //private Car[] cars = new Car[10];
+    //private int indexCar = 0;
+
+    private Toy[] toys = new Toy[20];  //ta tablica zastąpi dwie powyżze cars i bears
+    private int indexToy = 0;
+
 
     public void addBear(Bear bear) {
-        if (indexBear < bears.length) {
-            bears[indexBear++] = bear;
+        if (indexToy < toys.length) {    //mogę zastąpić misia toysem, czyli przypiac misia do zabawek, bo miś jest klasą pochodnę do klasy toys
+            toys[indexToy++] = bear;
         }
     }
 
     public void addCar(Car car) {
-        if (indexCar < cars.length) {
-            cars[indexCar++] = car;
+        if (indexToy < toys.length) {
+            toys[indexToy++] = car;
         }
     }
 
     public void displayAll() {
-        for (int i = 0; i < indexBear; i++) {
-            bears[i].display();
+        for (int i = 0; i < indexToy; i++) {
+            toys[i].display();
             System.out.println();
         }
-        for (int i = 0; i < indexCar; i++) {
-            cars[i].display();
-            System.out.println();
-        }
+        //for (int i = 0; i < indexBear; i++) {
+        //   bears[i].display();
+        //    System.out.println();
+        //}
+        //for (int i = 0; i < indexCar; i++) {
+        //    cars[i].display();
+        //    System.out.println();
+        //}
     }
 }
